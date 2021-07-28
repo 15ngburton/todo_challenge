e098f9a69cfd585b0e98627e76e30746d0ce58c3
Nathan Burton <you@example.com>
2021-07-27 20:35:19 -0600

    Added model

---
+++ backend/db/migrate/20210728022937_create_todo_items.rb
@@ -0,0 +1,11 @@
+class CreateTodoItems < ActiveRecord::Migration[6.1]
+  def change
+    create_table :todo_items do |t|
+      t.string :description
+      t.boolean :complete
+      t.boolean :active
+
+      t.timestamps
+    end
+  end
+end
